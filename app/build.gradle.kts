42f0a9785b57862df014fab7f264c0dd63a5d277
Oleg Loginov <o.loginov@yadro.com>
2024-11-11 09:06:08 +0300

    Implement heavy function

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "ru.otus.coroutines"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "ru.otus.coroutines"
--- app/src/main/java/ru/otus/coroutines/first/SomePresenter.kt
+++ app/src/main/java/ru/otus/coroutines/first/SomePresenter.kt
@@ -1,17 +1,52 @@
 package ru.otus.coroutines.first
 
-class SomePresenter {
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
+class SomePresenter {
+    private val scope = CoroutineScope(Dispatchers.IO)
     /**
      * 1. Реализуйте получение данных из метода BlockingRepository#getHeavyData
      * 2. Если BlockingRepository#getHeavyData не ответит в течение 5 секунд, необходимо
      * заэмитить в LiveData/StateFlow объект Error
      * 3. При успешном сценарии пробросьте в в LiveData/StateFlow объект Success
      */
-    fun populateHeavyData() {}
+    fun populateHeavyData(): Flow<Result> {
+        return channelFlow {
+            val heavyJob = scope.launch {
+                BlockingRepository().getHeavyData()
+                if (isActive) {
+                    send(Success)
+                }
+            }
+            delay(5000)
+            if (heavyJob.isActive) {
+                heavyJob.cancel()
+                send(Error(Exception()))
+            }
+        }
+    }
 
 
     sealed interface Result
     data object Success : Result
     data class Error(val exception: Exception) : Result
+
+}
+
+fun main() {
+    println("Main start")
+    runBlocking {
+        SomePresenter().populateHeavyData().first().let {
+            println("Result: $it")
+        }
+    }
+    println("Main end")
 }
